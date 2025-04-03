a1f17ec6af99d7e19ec597dedc4bed45b11a2b8c
keshav660 <keshavbhatt660@gmail.com>
2025-04-03 22:09:34 +0530

    resolved merge conflicts

--- 6Patterns/6_inverted_rotated_half_pyramid.cpp
+++ 6Patterns/6_inverted_rotated_half_pyramid.cpp
@@ -1,12 +1,24 @@
+/*
+    *
+   **
+  ***
+ ****
+*****
+*/
 #include <iostream>
 using namespace std;
 int main()
 {
-    for (int i = 1; i <= 5; i++)
+    int n = 5 ;
+    for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; j <= i; j++)
+        for (int j = 1; j <= n-i; j++)
         {
-            cout << "* ";
+            cout << " ";
+        }
+        for (int j = 1; j <=  i; j++)
+        {
+            cout << "*";
         }
         cout << endl;
     }
