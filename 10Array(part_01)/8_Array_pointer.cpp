023cfc0ee51465fd0214f17e5dbee3f57c1bc263
keshav660 <keshavbhatt660@gmail.com>
2025-06-28 08:47:37 +0530

    array program

---
+++ 10Array(part_01)/8_Array_pointer.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int arr[5] = {1, 2, 3, 4, 5}; //array treated as a constent pointer 
+ int a = 25 ; 
+//  arr = &a ; // not possible , because arr is an array and a is a pointer
+    return 0;
+}
